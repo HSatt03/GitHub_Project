e5a4f2bf1922c314ed00dac42b5f3bd2dd58b07d
Hasti <hsattari2006@gmail.com>
2025-03-27 20:48:58 +0330

    new commit

---
+++ Test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Lesson
+{
+private:
+    Lesson(string = "Wourld",string = "Hasti", string = "sdfghjhgfdfgh", int = 120000);
+    void setPrice(int price)
+    {
+        if(price > 0)
+        {
+            _price = price;
+        }
+    }
+public:
+    string _name;
+    string _author;
+    string _ISBN;
+    int _price;
+};
+Lesson::Lesson(string name, string author, string ISBN, int price)
+{
+    _name = name;
+    _author = author;
+    _ISBN = ISBN;
+    setPrice(price);
+}
+int main()
+{
+
+    return ;
+}
